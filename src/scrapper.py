f8eac9a14d150211c33a7f8b5e533b2e42527e96
Justin Augustine <justinraugustine@gmail.com>
2025-03-19 22:38:22 +0000

    Improved robustness to errors in lookup and ran code for all events for UTS 2023 and 2024

--- src/scrapper.py
+++ src/scrapper.py
@@ -9,18 +9,17 @@ import lxml
 def extract_details(raw_xml):
     global category_count
     obj ={}
-    obj["Surname"] = raw_xml["nom"]
-    obj["FirstNames"] = raw_xml["prenom"]
-    obj["Gender"] = raw_xml["sx"]
-    try:
-        obj["Country"] = raw_xml["cio"]
-    except Exception as e:
-        obj["Country"] = ""
-    obj["TotalTime"] = raw_xml["tps"]
-    obj["Category"] = raw_xml["cat"]
+    obj["BibNumber"] = raw_xml["doss"] if "doss" in raw_xml.attrs else ""
+    obj["Surname"] = raw_xml["nom"] if "nom" in raw_xml.attrs else ""
+    obj["FirstNames"] = raw_xml["prenom"] if "prenom" in raw_xml.attrs else ""
+    obj["Gender"] = raw_xml["sx"] if "sx" in raw_xml.attrs else ""
+    obj["Country"] = raw_xml["cio"] if "cio" in raw_xml.attrs else ""
+    obj["TotalTime"] = raw_xml["tps"] if "tps" in raw_xml.attrs else ""
+    obj["Category"] = raw_xml["cat"] if "cat" in raw_xml.attrs else ""
     update_category_count(raw_xml["cat"])
-    obj["CategoryPosition"] = category_count[raw_xml["cat"]]
+    obj["CategoryPosition"] = category_count[raw_xml["cat"]] if "cat" in raw_xml.attrs else ""
     return obj
+
 def update_category_count(category):
     global category_count
     if category in category_count:
@@ -28,27 +27,32 @@ def update_category_count(category):
     else:
         category_count[category] = 1  # Add element with a value of 1
 
-# TODO
-# UTMB index - done
-# splits
-# where they came overall
-# where they came in their age category
-race_results_url = "https://livetrail.net/histo/uts_2023/teteCourse.php?course=50k&cat=scratch&mode=full"
-ind_racer_url = "https://livetrail.net/histo/uts_2023/coureur.php?rech="
+def scrape_checkpoints(raw_xml):
+    points = {}
+    for p in raw_xml.find_all('points')[0].find_all('p'):
+        if p["idpt"] == '0':
+            points[p["idpt"]] = "Start"
+        else:
+            points[p["idpt"]] = p["n"]
+    return points
+
+
+race_results_url = "https://livetrail.net/histo/uts_2024/teteCourse.php?course=100m&cat=scratch&mode=full"
+ind_racer_url = "https://livetrail.net/histo/uts_2024/coureur.php?rech="
 page = requests.get(race_results_url)
 print(page.content)
 
-soup = BeautifulSoup(page.text, "lxml")
+soup = BeautifulSoup(page.text, "lxml-xml")
+invalid_url_text = soup.find(string="requete1 invalide") # love the french
+if invalid_url_text:
+    print("Invalid Race Results URL: " + race_results_url)
+    quit()
+
 data = {}
 counter = 1
 category_count = {}
 
-points = {}
-for p in soup.find_all('points')[0].find_all('p'):
-    if p["idpt"] == '0':
-        points[p["idpt"]] = "Start"
-    else:
-        points[p["idpt"]] = p["n"]
+points = scrape_checkpoints(soup)
 
 print(points)
 
@@ -58,18 +62,26 @@ for c in soup.find_all('c'):
             print(counter)
             useful_details = extract_details(c)
             page = requests.get(ind_racer_url + c["doss"])
-            soup2 = BeautifulSoup(page.text, "lxml")
-            try:
-                for palm in soup2.find_all('palm'):
-                    useful_details["UTMB"] = palm["cote"]
-            except Exception as e:
-                useful_details["UTMB"] = ""
-            for e in soup2.find_all('e'):
+            soup2 = BeautifulSoup(page.text, "lxml-xml")
+            # handle if the racer cannot be found - Could be a bad url
+            valid_url_attr = soup2.find("fiche")  # love the french
+            if not valid_url_attr:
+                print("Invalid Racer Results URL: " + ind_racer_url + c["doss"])
+                useful_details["UTMB"] = "ERR"
+                for p in points:
+                    useful_details[p] = "ERR"
+            else:
                 try:
-                    if e["idpt"] is not None:
-                        useful_details[points[e["idpt"]]] = e["tps"]
+                    for palm in soup2.find_all('palm'):
+                        useful_details["UTMB"] = palm["cote"]
                 except Exception as e:
-                    pass
+                    useful_details["UTMB"] = ""
+                for e in soup2.find_all('e'):
+                    try:
+                        if e["idpt"] is not None:
+                            useful_details[points[e["idpt"]]] = e["tps"]
+                    except Exception as e:
+                        pass
 
             useful_details["OverallPosition"] = counter
             data[c["doss"]] = useful_details
@@ -78,14 +90,14 @@ for c in soup.find_all('c'):
     except Exception as e:
         continue
 
-with open('UTS_2023.json', 'w', encoding='utf-8') as f:
+with open('UTS_2024_100m.json', 'w', encoding='utf-8') as f:
     json.dump(data, f, ensure_ascii=False, indent=4)
 
 df = pd.DataFrame(data).T
 
 df.columns = list(data[next(iter(data))].keys())
 
-excel_file = "UTS_2023.xlsx"  # Replace with your desired Excel file name
+excel_file = "UTS_2024_100m.xlsx"  # Replace with your desired Excel file name
 df.to_excel(excel_file, index=False)
 
 print(f"JSON data has been successfully exported to {excel_file}")
